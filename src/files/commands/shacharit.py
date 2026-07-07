fa2fd6df71fc4e696670cff3a5fab46efe8448b0
salandarman <salandarman@proton.me>
2026-07-07 09:46:01 +0300

    fixed logical issues on shacharit

--- src/files/commands/shacharit.py
+++ src/files/commands/shacharit.py
@@ -8,7 +8,5 @@ else:
     delta = abs(now.hour*60 + now.minute - 360)
     if now.hour < 6:
         print(f"{delta//60} hours, {delta%60} minutes left")
-    elif now.hour == 6 and now.minute > 30:
-        print(f"{(1440-delta)//60} hours, {(1440-delta)%60} minutes left")
     else:
         print(f"{(1440-delta)//60} hours, {(1440-delta)%60} minutes left")
